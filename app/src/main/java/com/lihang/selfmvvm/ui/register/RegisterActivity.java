3e83a76cf8f4fe5e1de180fd41dfef3dbf501b84
ScofieldZhang <302996244@qq.com>
2020-07-02 22:04:28 +0800

    add: 注册登录界面

---
+++ app/src/main/java/com/lihang/selfmvvm/ui/register/RegisterActivity.java
@@ -0,0 +1,16 @@
+package com.lihang.selfmvvm.ui.register;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.lihang.selfmvvm.R;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
